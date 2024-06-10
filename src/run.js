ee8910bf90f756baa0b0a655ea41eaa6bd604ab2
echizenyayota <virtualmisso30@gmail.com>
2024-06-10 18:01:48 +0900

    Add configuration to your discounts function Step 1: Configure the function

--- src/run.js
+++ src/run.js
@@ -1,7 +1,6 @@
 // @ts-check
 import { DiscountApplicationStrategy } from "../generated/api";
 
-// Use JSDoc annotations for type safety
 /**
 * @typedef {import("../generated/api").RunInput} RunInput
 * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
@@ -17,42 +16,51 @@ const EMPTY_DISCOUNT = {
   discounts: [],
 };
 
-// The configured entrypoint for the 'purchase.product-discount.run' extension target
 /**
 * @param {RunInput} input
 * @returns {FunctionRunResult}
 */
 export function run(input) {
+  // Define a type for your configuration, and parse it from the metafield
+  /**
+  * @type {{
+  *   quantity: number
+  *   percentage: number
+  * }}
+  */
+  const configuration = JSON.parse(
+    input?.discountNode?.metafield?.value ?? "{}"
+  );
+  if (!configuration.quantity || !configuration.percentage) {
+    return EMPTY_DISCOUNT;
+  }
+
   const targets = input.cart.lines
-  // Only include cart lines with a quantity of two or more
-  .filter(line => line.quantity >= 2 && line.merchandise.__typename === "ProductVariant")
-  .map(line => {
-    const merchandise = /** @type {ProductVariant} */ (line.merchandise);
-    return /** @type {Target} */ ({
-      // Use the cart line ID to create a discount target
-      productVariant: {
-        id: merchandise.id
-      }
+    // Use the configured quantity instead of a hardcoded value
+    .filter(line => line.quantity >= configuration.quantity && line.merchandise.__typename === "ProductVariant")
+    .map(line => {
+      const merchandise = /** @type {ProductVariant} */ (line.merchandise);
+      return /** @type {Target} */ ({
+        // Use the cart line ID to create a discount target
+        productVariant: {
+          id: merchandise.id
+        }
+      });
     });
-  });
 
   if (!targets.length) {
-    // You can use STDERR for debug logs in your function
     console.error("No cart lines qualify for volume discount.");
     return EMPTY_DISCOUNT;
   }
 
-  // The @shopify/shopify_function package applies JSON.stringify() to your function result
-  // and writes it to STDOUT
   return {
     discounts: [
       {
-        // Apply the discount to the collected targets
         targets,
-        // Define a percentage-based discount
         value: {
           percentage: {
-            value: "20.0"
+            // Use the configured percentage instead of a hardcoded value
+            value: configuration.percentage.toString()
           }
         }
       }
