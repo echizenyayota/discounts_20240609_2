3f333822e137b8ff4593ef1b9c1bab807f40f1ec
echizenyayota <virtualmisso30@gmail.com>
2024-06-10 16:43:55 +0900

    Build a discounts function Step 1: Create the product discount function

--- src/run.js
+++ src/run.js
@@ -1,27 +1,61 @@
 // @ts-check
 import { DiscountApplicationStrategy } from "../generated/api";
 
+// Use JSDoc annotations for type safety
 /**
- * @typedef {import("../generated/api").RunInput} RunInput
- * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
- */
+* @typedef {import("../generated/api").RunInput} RunInput
+* @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+* @typedef {import("../generated/api").Target} Target
+* @typedef {import("../generated/api").ProductVariant} ProductVariant
+*/
 
 /**
- * @type {FunctionRunResult}
- */
+* @type {FunctionRunResult}
+*/
 const EMPTY_DISCOUNT = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
   discounts: [],
 };
 
+// The configured entrypoint for the 'purchase.product-discount.run' extension target
 /**
- * @param {RunInput} input
- * @returns {FunctionRunResult}
- */
+* @param {RunInput} input
+* @returns {FunctionRunResult}
+*/
 export function run(input) {
-  const configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
-  );
+  const targets = input.cart.lines
+  // Only include cart lines with a quantity of two or more
+  .filter(line => line.quantity >= 2)
+  .map(line => {
+    return /** @type {Target} */ ({
+      // Use the cart line ID to create a discount target
+      cartLine: {
+        id: line.id
+      }
+    });
+  });
 
-  return EMPTY_DISCOUNT;
+  if (!targets.length) {
+    // You can use STDERR for debug logs in your function
+    console.error("No cart lines qualify for volume discount.");
+    return EMPTY_DISCOUNT;
+  }
+
+  // The @shopify/shopify_function package applies JSON.stringify() to your function result
+  // and writes it to STDOUT
+  return {
+    discounts: [
+      {
+        // Apply the discount to the collected targets
+        targets,
+        // Define a percentage-based discount
+        value: {
+          percentage: {
+            value: "10.0"
+          }
+        }
+      }
+    ],
+    discountApplicationStrategy: DiscountApplicationStrategy.First
+  };
 };
